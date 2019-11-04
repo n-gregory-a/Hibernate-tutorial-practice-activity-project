0d947ab8fb18ab19eef2c833ab08c86ba5883b97
n-gregory-a <ovonely@gmail.com>
2019-11-04 16:08:15 +0700

    Hibernate tutorial practice activity: - Set up the Hibernate configuration file - Create Employee class (entity) with Hibernate annotations

---
+++ src/main/java/gn/hibernatePractice/entity/Employee.java
@@ -0,0 +1,70 @@
+package gn.hibernatePractice.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String company;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String company) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.company = company;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", company='" + company + '\'' +
+                '}';
+    }
+}
